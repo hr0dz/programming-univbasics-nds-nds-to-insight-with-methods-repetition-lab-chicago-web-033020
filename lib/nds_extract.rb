4f3445892cb50a5742f2fac661d0f60de68f083c
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-26 23:01:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -59,13 +59,11 @@ def total_gross(source)
   directornames = list_of_directors(source)
   total = 0
   index = 0
+  binding.pry
   while index < grosstotals.length do
     x_value = grosstotals[index][:name]
     total += grosstotals[index][:name]
     index += 1
     end
-    binding.pry
 total
 end
-
-
