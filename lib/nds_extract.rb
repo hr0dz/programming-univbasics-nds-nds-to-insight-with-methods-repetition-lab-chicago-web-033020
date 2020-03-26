54831f78d0aed07045204603946b3cec3d4178f1
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-26 22:44:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -53,6 +53,7 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
   directors_totals(source)
+  list_of_directors(source)
   
   
   
